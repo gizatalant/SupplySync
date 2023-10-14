a48020a939fa604aea9c258ba3710b1a8433337e
Zhaiyl Ertabyldiev <zhaiylertabyldiev@Zhaiyls-MacBook-Pro.local>
2023-10-14 13:30:56 -0700

    refactored editCompany

--- src/test/java/tests/SuplySync/TestPOM.java
+++ src/test/java/tests/SuplySync/TestPOM.java
@@ -1,45 +1,57 @@
 package tests.SuplySync;
 
+import com.github.javafaker.Faker;
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utilities.Driver;
+import utilities.UtilWait;
+
+import java.security.Key;
+import java.time.Duration;
 
 public class TestPOM {
 
     WebDriver driver;
+
     @BeforeTest
     public void before() {
         driver = Driver.getDriver();
     }
 
-    @Test (priority = 1)
+    @Test(priority = 1)
     public void loginPage() {
         driver.get("https://supplysync.us/login");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
     }
 
-    @Test (priority = 2)
+    @Test(priority = 2)
     public void invalidEmail() {
         driver.get("https://supplysync.us/login");
         LoginPage loginPage = new LoginPage();
         loginPage.invalidEmail();
-        if (loginPage.errorMessage.isDisplayed()){
+        if (loginPage.errorMessage.isDisplayed()) {
             System.out.println("Login or password is incorrect, try again");
-        }
-        else{
+        } else {
             System.out.println("You have successfully logged in");
         }
     }
 
-    @Test (priority = 3)
-    public void testEditCompanyInfo() {
+    @Test(priority = 3)
+    public void testEditCompanyInfo() throws InterruptedException {
         driver.get("https://supplysync.us/dashboard/companies");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         LoginPage loginPage = new LoginPage();
         loginPage.login();
+        Faker faker = new Faker();
 
         CompaniesPage companiesPage = new CompaniesPage();
         companiesPage.companiesSectionButton.click();
@@ -47,49 +59,65 @@ public class TestPOM {
         companiesPage.editButton.click();
 
         companiesPage.name.click();
+        Thread.sleep(2000);
         for (int i = 0; i < 30; i++) {
             companiesPage.name.sendKeys(Keys.BACK_SPACE);
         }
-        companiesPage.name.sendKeys("Codewise");
+        String fakeCompany = faker.company().name();
+        companiesPage.name.sendKeys(fakeCompany);
+
 
         companiesPage.email.click();
         for (int i = 0; i < 30; i++) {
             companiesPage.email.sendKeys(Keys.BACK_SPACE);
         }
-        companiesPage.email.sendKeys("codewise@gmail.com");
+        String fakeEmail = faker.internet().emailAddress();
+        companiesPage.email.sendKeys(fakeEmail);
 
         companiesPage.address.click();
         for (int i = 0; i < 30; i++) {
             companiesPage.address.sendKeys(Keys.BACK_SPACE);
         }
-        companiesPage.address.sendKeys("Chicago City");
+        String fakeAddress = faker.country().capital();
+        companiesPage.address.sendKeys(fakeAddress);
 
         companiesPage.phoneNumber.click();
         for (int i = 0; i < 30; i++) {
             companiesPage.phoneNumber.sendKeys(Keys.BACK_SPACE);
         }
-        companiesPage.phoneNumber.sendKeys("555555555");
+        String fakePhoneNumber = faker.phoneNumber().phoneNumber();
+        companiesPage.phoneNumber.sendKeys(fakePhoneNumber);
 
         companiesPage.saveButton.click();
 
         String pageSource = driver.getPageSource();
 
-        if (pageSource.contains("Codewise")) {
+        if (pageSource.contains(fakeCompany)) {
             System.out.println("Name verified");
         }
-        if (pageSource.contains("Chicago city")) {
+        if (pageSource.contains(fakeAddress)) {
             System.out.println("Address verified");
         }
-        if (pageSource.contains("+996 555 555 555")) {
+        if (pageSource.contains(fakePhoneNumber)) {
             System.out.println("Number verified");
         }
-        if (pageSource.contains("codewise@gmail.com")) {
+        if (pageSource.contains(fakeEmail)) {
             System.out.println("Email verified");
+        }
+
+        WebElement verificationMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-caiLqq cYMnth']")));
+        String messageText = verificationMessage.getText();
+
+        System.out.println("Actual Message: " + messageText);
+
+        if (messageText.equals("Company successfully changed")) {
+            System.out.println("Verified");
+        } else {
+            System.out.println("Not verified");
         }
     }
 
-
-    @Test (priority = 4)
+    @Test(priority = 4)
     public void testEditMastersName() throws InterruptedException {
 
         driver.get("https://supplysync.us/dashboard/masters");
@@ -109,7 +137,7 @@ public class TestPOM {
 
     }
 
-    @Test (priority = 5)
+    @Test(priority = 5)
     public void testMasterNameNumbersAndSymbols() throws InterruptedException {
 
         driver.get("https://supplysync.us/dashboard/masters");
@@ -127,7 +155,7 @@ public class TestPOM {
         System.out.println("Numbers and symbols allowed");
     }
 
-    @Test (priority = 6)
+    @Test(priority = 6)
     public void testMastersNameUpperAndLowercase() throws InterruptedException {
 
         driver.get("https://supplysync.us/dashboard/masters");
@@ -146,38 +174,36 @@ public class TestPOM {
         System.out.println("The master's name can be changed to uppercase and lowercase");
     }
 
-    @Test (priority = 7)
-    public void blockTheMasters(){
+    @Test(priority = 7)
+    public void blockTheMasters() {
         driver.get("https://supplysync.us/dashboard/masters");
         MastersPage mastersPage = new MastersPage();
         mastersPage.blockThreeDot.click();
         mastersPage.block.click();
-        if (mastersPage.mastersField.isDisplayed()){
+        if (mastersPage.mastersField.isDisplayed()) {
             System.out.println("Master blocked");
-        }
-        else {
+        } else {
             System.out.println("Error");
         }
     }
 
-    @Test (priority = 8)
-    public void unlockTheMasters(){
+    @Test(priority = 8)
+    public void unlockTheMasters() {
         driver.get("https://supplysync.us/dashboard/masters");
         MastersPage mastersPage = new MastersPage();
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,250)", "");
         mastersPage.unlockThreeDot.click();
         mastersPage.unlock.click();
-        if (mastersPage.mastersField.isDisplayed()){
+        if (mastersPage.mastersField.isDisplayed()) {
             System.out.println("Master unlocked");
-        }
-        else {
+        } else {
             System.out.println("Error");
         }
     }
 
 
-    @Test (priority = 9)
+    @Test(priority = 9)
     public void logout() {
 
         LogoutPage logoutPage = new LogoutPage();
